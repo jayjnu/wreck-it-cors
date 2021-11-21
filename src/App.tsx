32a8daf5059327d30634d0cecbc91cc364f96e33
jayjnu <doublejnu@gmail.com>
2021-11-21 21:28:04 +0900

    feat(App): add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Typography } from '@mui/material';
 import { FC } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Form } from './features/FetchValidator';
+import { SameOriginValidator } from './features/SameOriginValidator';
 
 const App: FC = () => {
   return (
     <div className="App">
       <Typography variant="h1">Wreck it CORS!</Typography>
-      <Form />
+      <Routes>
+        <Route path="/" element={<SameOriginValidator />} />
+        <Route path="/fetch" element={<Form />} />
+      </Routes>
     </div>
   );
 };
