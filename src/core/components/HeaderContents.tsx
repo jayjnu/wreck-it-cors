d829048e1e361d99970d9b88751014ba0afed237
jayjnu <doublejnu@gmail.com>
2021-11-26 20:39:00 +0900

    fix(SameOriginValidator): 로고 상단 여백 추가

--- src/core/components/HeaderContents.tsx
+++ src/core/components/HeaderContents.tsx
@@ -2,14 +2,15 @@ import { styled } from '@mui/material';
 import { memo } from 'react';
 import { Logo } from './Logo';
 
-const HeaderWrapper = styled('div')({
-  paddingBottom: 30
-});
+const HeaderWrapper = styled('div')(({ theme }) => ({
+  paddingBottom: theme.spacing(5)
+}));
 
-const Bricks = styled('div')({
+const Bricks = styled('div')(({ theme }) => ({
   backgroundImage: 'url(/bricks.jpg)',
-  height: 40
-});
+  height: 40,
+  marginBottom: theme.spacing(5)
+}));
 
 export const HeaderContents = memo(function HeaderContents() {
   return (
